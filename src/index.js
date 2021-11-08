348dc763ad77279be57a50748a6de51c3fadcbf1
Taras Boikov <tarasgowest@gmail.com>
2021-11-08 23:29:47 +0200

    Реализует подгрузку популярных фильмов на главную (первую) страницу

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './sass/main.scss';
 
+
+
+//От Тараса
 // import './js/add-movie-list.js';
+// import './js/apiPopular.js';
--- src/js/add-movie-list.js
+++ src/js/add-movie-list.js
@@ -1,3 +1,23 @@
 import renderList from '../templates/movie-list.hbs';
+import apiPopular from './apiPopular';
+
 const movieList = document.querySelector('.movie-list');
-movieList.insertAdjacentHTML('beforeend', renderList());
+
+
+function getMovie() {
+    apiPopular()
+        .then(response => {
+            return response.json()
+        })
+        .then(movie => {
+            const renderMovie = renderList(movie);
+            movieList.insertAdjacentHTML('beforeend', renderMovie);
+
+            
+        }).catch(error => {
+            console.log("404")
+            console.log(error);
+        })
+
+};
+getMovie();
