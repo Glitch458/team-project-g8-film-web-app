2c40cdb4949786bacda93cc1e141e37fdde41e8f
Andrey Katrych <andrew.katrich@gmail.com>
2021-11-11 19:47:10 +0200

    some fixes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,21 @@
 import movieCardTpl from '../templates/movie-card.hbs';
 
 const refs = {
-    movieCardContainer: document.querySelector('.movie-container')
-}
-
+  movieCardContainer: document.querySelector('.movie-container'),
+};
 
 function fetchMovie(movie) {
-    return fetch('https://api.themoviedb.org/3/movie/255?api_key=744d4295a955a17cccf78658c430f199')
+  return fetch('https://api.themoviedb.org/3/movie/255?api_key=744d4295a955a17cccf78658c430f199')
     .then(response => {
-        return response.json();
+      return response.json();
     })
     .then(movie => {
-        console.log(movie);
-        const markup = movieCardTpl(movie);
-        console.log(markup)
-        refs.movieCardContainer.innerHTML = markup;
+      const markup = movieCardTpl(movie);
+      console.log(markup);
+      refs.movieCardContainer.innerHTML = markup;
     })
-    .catch(eror => console.log(error))
+    .catch(eror => console.log(error));
 }
-console.log(fetchMovie())
 
 // function renderMovieCard() {
 //     const markup = modalCard(response);
