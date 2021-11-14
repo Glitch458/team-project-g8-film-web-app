a18d34c4538ad02e5f6fd757ee65b19ed01f166e
l-Bohdan-l <bpapushoj@gmail.com>
2021-11-14 22:36:00 +0200

    create functional scroll up btn

--- src/js/scroll-up-btn.js
+++ src/js/scroll-up-btn.js
@@ -1,7 +1,8 @@
-// When the user scrolls down 20px from the top of the document, show the button
 const scrollUpBtn = document.querySelector('.scroll-up-btn')
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -12,9 +13,17 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function topFunction(e) {
+    e.preventDefault()
+    document.body.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })  // For Safari
+  document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    }) 
+    // For Chrome, Firefox, IE and Opera
 }
 
 scrollUpBtn.addEventListener('click', topFunction)
