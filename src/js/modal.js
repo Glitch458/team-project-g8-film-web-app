38f093f33f42fd532c33a62b4b8c2bad7a2f20f5
Ruslan-GH <82412111+Ruslan-GH@users.noreply.github.com>
2021-11-15 20:38:44 +0200

    Додано відкриття та закриття модалки
    Без Кнопки!

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,13 @@
 import movieCardTpl from '../templates/movie-card.hbs';
+import { backdropEl, openModal, closeModal } from './backdrop';
 
 const refs = {
-  movieCardContainer: document.querySelector('.movie-container'),
+  movieCardContainer: document.querySelector('.modal-movie-container'),
+  galleryMovies: document.querySelector('.gallery'),
+  closeBtn: document.querySelector('.modal-card-close-btn')
 };
+
+console.log(refs.closeBtn)
 
 function fetchMovie(movie) {
   return fetch('https://api.themoviedb.org/3/movie/777?api_key=744d4295a955a17cccf78658c430f199')
@@ -15,5 +20,31 @@ function fetchMovie(movie) {
     })
     .catch(error => console.log(error));
 }
+// console.log(refs.closeBtn)
 
-fetchMovie();
+refs.galleryMovies.addEventListener('click', openMovieModal);
+
+function openMovieModal(evt) {
+  evt.preventDefault();
+  openModal()
+  fetchMovie();
+  window.addEventListener('keydown', closeContributorsModalByEsc);
+  backdropEl.addEventListener('click', closeContributorsModalByClick);
+}
+
+function closeContributorsModalByEsc(evt) {
+  if (evt.key === 'Escape') {
+    closeModal();
+    window.removeEventListener('keydown', closeContributorsModalByEsc);
+        backdropEl.removeEventListener('click', closeContributorsModalByClick);
+  }
+}
+
+function closeContributorsModalByClick(evt) {
+  if (evt.target === backdropEl) {
+    closeModal();
+    backdropEl.removeEventListener('click', closeContributorsModalByClick);
+    window.removeEventListener('keydown', closeContributorsModalByEsc);
+
+  }
+}
