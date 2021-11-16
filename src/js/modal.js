8753c700009c206fce952b27de72951ac9a5c513
Taras Boikov <tarasgowest@gmail.com>
2021-11-17 00:56:10 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,4 @@
-export default fetchMovie();
-import movieCardTpl from '../templates/movie-card.hbs';
-import { backdropEl, openModal, closeModal } from './backdrop';
-
-
-const refs = {
-  movieCardContainer: document.querySelector('.modal-movie-container'),
-  galleryMovies: document.querySelector('.gallery'),
-  // closeBtn: document.querySelector('.close-btn')
-};
-
-console.log('кнопка закрытия', refs.movieCardContainer)
-
-function fetchMovie(movie) {
+export default function fetchMovie(movie) {
   return fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=744d4295a955a17cccf78658c430f199`)
     .then(response => {
       return response.json();
@@ -22,7 +9,17 @@ function fetchMovie(movie) {
       refs.movieCardContainer.innerHTML = markup;
     })
     .catch(error => console.log(error));
-}
+ };
+import movieCardTpl from '../templates/movie-card.hbs';
+import { backdropEl, openModal, closeModal } from './backdrop';
+
+
+const refs = {
+  movieCardContainer: document.querySelector('.modal-movie-container'),
+  galleryMovies: document.querySelector('.gallery'),
+};
+
+
 
 refs.galleryMovies.addEventListener('click', openMovieModal);
 
