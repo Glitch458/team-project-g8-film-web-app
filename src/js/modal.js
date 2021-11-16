363f6ff1e3c82a805f78e93ea8deb492e97ddaf2
Taras Boikov <tarasgowest@gmail.com>
2021-11-17 00:01:03 +0200

    Ровняет модалку и добавляет слушателей на кнопки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,14 @@
 import movieCardTpl from '../templates/movie-card.hbs';
 import { backdropEl, openModal, closeModal } from './backdrop';
 
+
 const refs = {
   movieCardContainer: document.querySelector('.modal-movie-container'),
   galleryMovies: document.querySelector('.gallery'),
-  closeBtn: document.querySelector('.modal-card-close-btn')
+  // closeBtn: document.querySelector('.close-btn')
 };
 
-console.log(refs.closeBtn)
+console.log('кнопка закрытия', refs.movieCardContainer)
 
 function fetchMovie(movie) {
   return fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=744d4295a955a17cccf78658c430f199`)
@@ -21,8 +22,6 @@ function fetchMovie(movie) {
     })
     .catch(error => console.log(error));
 }
-// console.log(refs.closeBtn)
-
 
 refs.galleryMovies.addEventListener('click', openMovieModal);
 
@@ -39,7 +38,7 @@ function closeContributorsModalByEsc(evt) {
   if (evt.key === 'Escape') {
     closeModal();
     window.removeEventListener('keydown', closeContributorsModalByEsc);
-        backdropEl.removeEventListener('click', closeContributorsModalByClick);
+    backdropEl.removeEventListener('click', closeContributorsModalByClick);
   }
 }
 
@@ -51,3 +50,20 @@ function closeContributorsModalByClick(evt) {
 
   }
 }
+// функции для кнопок модалки
+refs.movieCardContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('mobile-menu__icon')) {
+    // Кнопка закрытия модалки
+    closeModal();
+    window.removeEventListener('keydown', closeContributorsModalByEsc);
+    backdropEl.removeEventListener('click', closeContributorsModalByClick);
+  } else if (e.target.classList.contains('modal-card-btn-watched' || 'modal-card-btn-text-first')) {
+    //Функция для кнопки "ADD TO WACHED"
+    console.log('кнопка 1');
+  } else if (e.target.classList.contains('modal-card-btn-queue' || 'modal-card-btn-text-second')) {
+    // Функция для кнопки "ADD TO QUEUE"
+    console.log('кнопка 2');
+  }
+});
+
+
